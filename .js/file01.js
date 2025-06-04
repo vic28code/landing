7fb5df22592e7d5d48831055b2f3c5904c07dfeb
paul123 <139300207+paul2723@users.noreply.github.com>
2025-06-04 15:45:39 +0000

    cambios6

--- .js/file01.js
+++ .js/file01.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const showToast = () => {
+    const toast = document.getElementById("toast-interactive");
+    if (toast) {
+        toast.classList.add("md:block");
+    }
+};
+
+(() => {
+    showToast();
+})();
--- js/file01.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-(() => {
-    alert("¡Bienvenido a la página!");
-    console.log("Mensaje de bienvenida mostrado.");
-})();
